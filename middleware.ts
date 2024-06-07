651b53c1ea35714fd50d52c1784c6aa52f33a1ef
taekwon <universe134679@gmail.com>
2024-06-08 02:31:47 +0900

    middleware Setting

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,50 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const secretKey = process.env.CLERK_SECRET_KEY;
+// Define the protected routes
+const isProtectedRoute = createRouteMatcher([
+  "/",
+]);
 
-if (!secretKey) {
-  throw new Error('@clerk/nextjs: Missing secretKey.')
-}
+export default clerkMiddleware((auth, req) => {
+  const { userId, orgId, redirectToSignIn } = auth();
 
-export default clerkMiddleware({ secretKey });
+  if (!userId && req.nextUrl.pathname === "/") {
+    return NextResponse.next();
+  }
+
+  // Redirect to sign-in if user is not auth and trying to access a protected route
+  // In this case, all the routes are protected
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  // Redirect to auth user's organization page or selection page
+  if (userId && isProtectedRoute(req)) {
+    let path = "/select-org";
+
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  // Prevent unauth user's access
+  if (!userId && !isProtectedRoute(req)) {
+    return NextResponse.next();
+  }
+
+  // Redirect auth users without an organization ID to the organization selection page
+  if (userId && !orgId && req.nextUrl.pathname !== "/select-org") {
+    const orgSelection = new URL("/select-org", req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  // return NextResponse.next();
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
