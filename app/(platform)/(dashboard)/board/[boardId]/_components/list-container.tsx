84efbc8a8a0d6936ba5411333ea965daf4648968
taekwon <universe134679@gmail.com>
2024-06-16 01:55:15 +0900

    List Component -ing

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { ListWithCards } from "@/types";
+
+interface ListContainerProps {
+  data: ListWithCards[];
+  boardId: string;
+};
+
+export const ListContainer = ({
+  data,
+  boardId,
+}: ListContainerProps) => {
+  return (
+    <div>
+      List Container
+    </div>
+  );
+};
+
